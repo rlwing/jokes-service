d0c36876aeafc29150317f670abd7ecd9e561fbd
rlwing <rob@wingjr.com>
2019-05-15 11:53:11 -0400

    Added seed data for testing

---
+++ src/main/resources/data.sql
@@ -0,0 +1,17 @@
+/*
+-- Query: SELECT * FROM jokes.jokes
+LIMIT 0, 1000
+
+-- Date: 2019-05-08 07:42
+*/
+INSERT INTO `jokes` (`joke_id`,`category`,`joke`,`source`) VALUES (1834,'KIDJOKES','Why did the chicken cross the road?\nTo get to the other side!','Postman');
+INSERT INTO `jokes` (`joke_id`,`category`,`joke`,`source`) VALUES (1835,'KIDJOKES','Dirty Joke: The boy fell into the mud.\nClean Joke: He took a shower!','Postman');
+INSERT INTO `jokes` (`joke_id`,`category`,`joke`,`source`) VALUES (1836,'DADJOKES','My wife is so negative. I remembered the car seat, the stroller, AND the diaper bag. Yet all she can talk about is how I forgot the baby.!','Postman');
+INSERT INTO `jokes` (`joke_id`,`category`,`joke`,`source`) VALUES (1837,'DADJOKES','Q. Why did the scarecrow win an award?\nA. Because he was outstanding in his field.','Postman');
+INSERT INTO `jokes` (`joke_id`,`category`,`joke`,`source`) VALUES (1838,'MOMJOKES','Don’t wake up mom! There are at least seven species who eat their young. Your mom may be one of them.','Postman');
+INSERT INTO `jokes` (`joke_id`,`category`,`joke`,`source`) VALUES (1839,'MOMJOKES','You know you’re a mom when…\n\n…picking up another human to smell their butt is not only normal but totally necessary','Postman');
+INSERT INTO `jokes` (`joke_id`,`category`,`joke`,`source`) VALUES (1840,'KNOCKKNOCK','Knock! Knock!\nWho’s there?\nSays.\nSays who?\nSays me, that’s who!','Postman');
+INSERT INTO `jokes` (`joke_id`,`category`,`joke`,`source`) VALUES (1841,'TECHNOLOGY','Q. How did the programmer die in the shower?\nA. He read the shampoo bottle instructions: Lather. Rinse. Repeat.','Postman');
+INSERT INTO `jokes` (`joke_id`,`category`,`joke`,`source`) VALUES (1842,'TECHNOLOGY','How many programmers does it take to change a light bulb?\nNone – It’s a hardware problem','Postman');
+INSERT INTO `jokes` (`joke_id`,`category`,`joke`,`source`) VALUES (1843,'TECHNOLOGY','Why do programmers always mix up Halloween and Christmas?\nBecause Oct 31 equals Dec 25.','Postman');
+INSERT INTO `jokes` (`joke_id`,`category`,`joke`,`source`) VALUES (1844,'TECHNOLOGY','Knock, knock.\nWho’s there?\nvery long pause….\nJava.','Postman');
